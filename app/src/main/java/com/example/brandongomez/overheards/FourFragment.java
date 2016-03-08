f36c8b64d838aca671f23697f94717c850a81441
Zirconia <dtoney@ucsc.edu>
2016-03-07 16:31:14 -0800

    darion's changes

--- app/src/main/java/com/example/brandongomez/overheards/FourFragment.java
+++ app/src/main/java/com/example/brandongomez/overheards/FourFragment.java
@@ -7,10 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
 
 import com.example.brandongomez.overheards.R;
 
 public class FourFragment extends Fragment {
+
     public FourFragment() {
         // Required empty public constructor
     }
@@ -18,12 +20,27 @@ public class FourFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_four, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        ProgressBar spinner;
+        spinner = (ProgressBar) view.findViewById(R.id.progressBar1);
+
+        //spinner.setVisibility(View.GONE);
+        //spinner.setVisibility(View.VISIBLE);
+
+    }
+
+
 }
--- app/src/main/java/com/example/brandongomez/overheards/MainActivity.java
+++ app/src/main/java/com/example/brandongomez/overheards/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.brandongomez.overheards;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -8,9 +10,17 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.widget.EditText;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.brandongomez.overheards.R;
 import com.example.brandongomez.overheards.OneFragment;
@@ -28,6 +38,9 @@ public class MainActivity extends AppCompatActivity {
     //private Toolbar toolbar;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,4 +93,24 @@ public class MainActivity extends AppCompatActivity {
             return mFragmentTitleList.get(position);
         }
     }
+
+    /*stores the text from the post box in a variable when user clicks post button
+     NEED TO CHANGE LATER TO WORK WITH DATABASE AND POST MESSAGE TO SERVER INSTEAD
+    */
+    public void postInfo(View v){
+        EditText inputMessage = (EditText) findViewById(R.id.postBox);
+        EditText inputLocation = (EditText) findViewById(R.id.editText);//location editText
+        String message = inputMessage.getText().toString();
+        //MAYBE USE CURRENT LOCATION IF USER DOES NOT ENTER a LOCATION
+        String location = inputLocation.getText().toString();
+        System.out.println(message);
+        System.out.println(location);
+    }
+
+    public void settingsView(View v){
+        Intent intent = new Intent(this, SettingsActivity.class);
+        startActivity(intent);//pass the cuisine to the search activity for searching
+
+    }
+
 }
